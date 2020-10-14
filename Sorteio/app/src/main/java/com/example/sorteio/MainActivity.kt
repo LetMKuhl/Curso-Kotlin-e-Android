5cf715ae3b85de967c1f92b1fde977b90ac4be2f
lehlagoo <letimoris@gmail.com>
2020-10-14 19:22:01 -0300

    Iterface sorteio

---
+++ Sorteio/app/src/main/java/com/example/sorteio/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.sorteio
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = et_escolha.text.toString()
+    }
+
+}
