8f4e3308a78a63536ed71bc357a46a676ff6e01f
lehlagoo <letimoris@gmail.com>
2020-10-14 21:03:10 -0300

    App de Sorteio pronto

--- Sorteio/app/src/main/java/com/example/sorteio/MainActivity.kt
+++ Sorteio/app/src/main/java/com/example/sorteio/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sortearNumero(view: View){
-        var texto = et_escolha.text.toString()
+
+        val texto = pt_numero.text.toString()
+        val resultado = tx_resultado.text
+
+
+
+        if (texto.isEmpty()){
+
+           pt_numero.error = "Você deve digitar um valor"
+
+            return
+
+        } else if (texto.toInt() < 0 || texto.toInt() > 50){
+
+            pt_numero.error = "Tem que ser um numero entre 0 e 50"
+
+            return
+
+        }
+
+        val numeroSorteado = Random.nextInt(0,51)
+
+        if (numeroSorteado == texto.toInt()){
+            tx_resultado.text = "Número sorteado: $numeroSorteado\nParabéns, seu número foi sorteado!"
+            tx_resultado.visibility = View.VISIBLE
+        } else {
+            tx_resultado.text = "Número sorteado: $numeroSorteado\nPoxa, não foi dessa vez que " +
+                                "você conseguiu.\nMas não desista, tente novamente!"
+            tx_resultado.visibility = View.VISIBLE
+        }
+
     }
 
 }
